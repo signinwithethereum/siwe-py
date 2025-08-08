7dc24f7cd85179a2f73878884db3cddb01fd8d57
encrypted <koyofrontend@protonmail.com>
2025-08-08 17:32:56 +0200

    Upload repo

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# siwe-py
-A Python implementation of Sign-In with Ethereum
+# Sign-In with Ethereum
+
+This package provides a Python implementation of EIP-4361: Sign In With Ethereum.
+
+## Installation
+
+SIWE can be easily installed in any Python project with pip:
+
+```bash
+pip install siwe
+```
+
+## Usage
+
+SIWE provides a `SiweMessage` class which implements EIP-4361.
+
+### Parsing a SIWE Message
+
+Parsing is done by initializing a `SiweMessage` object with an EIP-4361 formatted string:
+
+```python
+from siwe import SiweMessage
+message = SiweMessage.from_message(message=eip_4361_string)
+```
+
+Or to initialize a `SiweMessage` as a `pydantic.BaseModel` right away:
+
+```python
+message = SiweMessage(domain="login.xyz", address="0x1234...", ...)
+```
+
+### Verifying and Authenticating a SIWE Message
+
+Verification and authentication is performed via EIP-191, using the `address` field of the `SiweMessage` as the expected signer. The validate method checks message structural integrity, signature address validity, and time-based validity attributes.
+
+```python
+try:
+    message.verify(signature="0x...")
+    # You can also specify other checks (e.g. the nonce or domain expected).
+except siwe.ValidationError:
+    # Invalid
+```
+
+### Serialization of a SIWE Message
+
+`SiweMessage` instances can also be serialized as their EIP-4361 string representations via the `prepare_message` method:
+
+```python
+print(message.prepare_message())
+```
+
+## Example
+
+Parsing and verifying a `SiweMessage` is easy:
+
+```python
+try:
+    message: SiweMessage = SiweMessage(message=eip_4361_string)
+    message.verify(signature, nonce="abcdef", domain="example.com"):
+except siwe.ValueError:
+    # Invalid message
+    print("Authentication attempt rejected.")
+except siwe.ExpiredMessage:
+    print("Authentication attempt rejected.")
+except siwe.DomainMismatch:
+    print("Authentication attempt rejected.")
+except siwe.NonceMismatch:
+    print("Authentication attempt rejected.")
+except siwe.MalformedSession as e:
+    # e.missing_fields contains the missing information needed for validation
+    print("Authentication attempt rejected.")
+except siwe.InvalidSignature:
+    print("Authentication attempt rejected.")
+
+# Message has been verified. Authentication complete. Continue with authorization/other.
+```
+
+## Testing
+
+```bash
+poetry install
+git submodule update --init
+poetry run pytest
+```
+
+## See Also
+
+- [Sign-In with Ethereum: TypeScript](https://github.com/spruceid/siwe)
+- [Example SIWE application: login.xyz](https://login.xyz)
+- [EIP-4361 Specification Draft](https://eips.ethereum.org/EIPS/eip-4361)
+- [EIP-191 Specification](https://eips.ethereum.org/EIPS/eip-191)
+
+## Disclaimer
+
+Our Python library for Sign-In with Ethereum has not yet undergone a formal
+security audit. We welcome continued feedback on the usability, architecture,
+and security of this implementation.
---
+++ tests/test_siwe.py
@@ -0,0 +1,165 @@
+import json
+import os
+
+import pytest
+from eth_account import Account, messages
+from humps import decamelize
+from web3 import HTTPProvider
+from pydantic import ValidationError
+
+from siwe.siwe import SiweMessage, VerificationError, datetime_from_iso8601_string
+
+BASE_TESTS = "tests/siwe/test/"
+with open(BASE_TESTS + "parsing_positive.json", "r") as f:
+    parsing_positive = decamelize(json.load(fp=f))
+with open(BASE_TESTS + "parsing_negative.json", "r") as f:
+    parsing_negative = decamelize(json.load(fp=f))
+with open(BASE_TESTS + "parsing_negative_objects.json", "r") as f:
+    parsing_negative_objects = decamelize(json.load(fp=f))
+with open(BASE_TESTS + "verification_negative.json", "r") as f:
+    verification_negative = decamelize(json.load(fp=f))
+with open(BASE_TESTS + "verification_positive.json", "r") as f:
+    verification_positive = decamelize(json.load(fp=f))
+with open(BASE_TESTS + "eip1271.json", "r") as f:
+    verification_eip1271 = decamelize(json.load(fp=f))
+
+endpoint_uri = "https://cloudflare-eth.com"
+try:
+    uri = os.environ["WEB3_PROVIDER_URI"]
+    if uri != "":
+        endpoint_uri = uri
+except KeyError:
+    pass
+sepolia_endpoint_uri = "https://rpc.sepolia.org"
+try:
+    uri = os.environ["WEB3_PROVIDER_URI_SEPOLIA"]
+    if uri != "":
+        sepolia_endpoint_uri = uri
+except KeyError:
+    pass
+
+
+class TestMessageParsing:
+    @pytest.mark.parametrize("abnf", [True, False])
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in parsing_positive.items()],
+    )
+    def test_valid_message(self, abnf, test_name, test):
+        siwe_message = SiweMessage.from_message(message=test["message"], abnf=abnf)
+        for key, value in test["fields"].items():
+            v = getattr(siwe_message, key)
+            if not (isinstance(v, int) or isinstance(v, list) or v is None):
+                v = str(v)
+            assert v == value
+
+    @pytest.mark.parametrize("abnf", [True, False])
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in parsing_negative.items()],
+    )
+    def test_invalid_message(self, abnf, test_name, test):
+        with pytest.raises(ValueError):
+            SiweMessage.from_message(message=test, abnf=abnf)
+
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in parsing_negative_objects.items()],
+    )
+    def test_invalid_object_message(self, test_name, test):
+        with pytest.raises(ValidationError):
+            SiweMessage(**test)
+
+
+class TestMessageGeneration:
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in parsing_positive.items()],
+    )
+    def test_valid_message(self, test_name, test):
+        siwe_message = SiweMessage(**test["fields"])
+        assert siwe_message.prepare_message() == test["message"]
+
+
+class TestMessageVerification:
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in verification_positive.items()],
+    )
+    def test_valid_message(self, test_name, test):
+        siwe_message = SiweMessage(**test)
+        timestamp = (
+            datetime_from_iso8601_string(test["time"]) if "time" in test else None
+        )
+        siwe_message.verify(test["signature"], timestamp=timestamp)
+
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in verification_eip1271.items()],
+    )
+    def test_eip1271_message(self, test_name, test):
+        if test_name == "loopring":
+            pytest.skip()
+        provider = HTTPProvider(endpoint_uri=endpoint_uri)
+        siwe_message = SiweMessage.from_message(message=test["message"])
+        siwe_message.verify(test["signature"], provider=provider)
+
+    def test_safe_wallet_message(self):
+        message = "localhost:3000 wants you to sign in with your Ethereum account:\n0x54D97AEa047838CAC7A9C3e452951647f12a440c\n\nPlease sign in to verify your ownership of this wallet\n\nURI: http://localhost:3000\nVersion: 1\nChain ID: 11155111\nNonce: gDj8rv7VVxN\nIssued At: 2024-10-10T08:34:03.152Z\nExpiration Time: 2024-10-13T08:34:03.249112Z"
+        signature = "0x"
+        # Use a Sepolia RPC node since the signature is generated on Sepolia testnet
+        # instead of mainnet like other EIP-1271 tests.
+        provider = HTTPProvider(endpoint_uri=sepolia_endpoint_uri)
+        siwe_message = SiweMessage.from_message(message=message)
+        siwe_message.verify(signature, provider=provider)
+
+    @pytest.mark.parametrize(
+        "provider", [HTTPProvider(endpoint_uri=endpoint_uri), None]
+    )
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in verification_negative.items()],
+    )
+    def test_invalid_message(self, provider, test_name, test):
+        if test_name in [
+            "invalidexpiration_time",
+            "invalidnot_before",
+            "invalidissued_at",
+        ]:
+            with pytest.raises(ValidationError):
+                siwe_message = SiweMessage(**test)
+            return
+        siwe_message = SiweMessage(**test)
+        domain_binding = test.get("domain_binding")
+        match_nonce = test.get("match_nonce")
+        timestamp = (
+            datetime_from_iso8601_string(test["time"]) if "time" in test else None
+        )
+        with pytest.raises(VerificationError):
+            siwe_message.verify(
+                test.get("signature"),
+                domain=domain_binding,
+                nonce=match_nonce,
+                timestamp=timestamp,
+                provider=provider,
+            )
+
+
+class TestMessageRoundTrip:
+    account = Account.create()
+
+    @pytest.mark.parametrize(
+        "test_name,test",
+        [(test_name, test) for test_name, test in parsing_positive.items()],
+    )
+    def test_message_round_trip(self, test_name, test):
+        message = SiweMessage(**test["fields"])
+        message.address = self.account.address
+        signature = self.account.sign_message(
+            messages.encode_defunct(text=message.prepare_message())
+        ).signature
+        message.verify(signature)
+
+    def test_schema_generation(self):
+        # NOTE: Needed so that FastAPI/OpenAPI json schema works
+        SiweMessage.model_json_schema()
